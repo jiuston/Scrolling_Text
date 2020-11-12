93874881aa1d36ef71d0460b3dfa92093349c304
Miguel Rodriguez <daw011.2020@gmail.com>
2020-11-12 13:45:22 +0100

    añadiendo la action bar contextual

--- app/src/main/java/jesuitas/dam/scrollingtext/MainActivity.java
+++ app/src/main/java/jesuitas/dam/scrollingtext/MainActivity.java
@@ -4,16 +4,57 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ActionMode;
 import android.view.ContextMenu;
+import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView articulo;
+    TextView subtitulo;
+    private ActionMode mActionmode;
+    private ActionMode.Callback mActionModeCallBack= new ActionMode.Callback() {
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+
+            MenuInflater inflater=getMenuInflater();
+            inflater.inflate(R.menu.menu_contet_barra,menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.edit_barra:
+                    displayToast("Editar de la barra");
+                    return true;
+                case R.id.Share_barra:
+                    displayToast("Share de la barra");
+                    return true;
+                //aqiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
+            }
+            return false;
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            displayToast(getString(R.string.saliendo));
+            mActionmode=null;
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +64,17 @@ public class MainActivity extends AppCompatActivity {
         //Asigno el menu contextual a esta View
         articulo=findViewById(R.id.article);
         registerForContextMenu(articulo);
+        subtitulo=findViewById(R.id.article_subheading);
+        subtitulo.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                if (mActionmode!=null) return false;
 
+                mActionmode= MainActivity.this.startActionMode(mActionModeCallBack);
+                v.setSelected(true);
+                return false;
+            }
+        });
     }
 
     @Override
@@ -44,10 +95,10 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             case R.id.Delete:
                 displayToast(getString(R.string.delete_text));
-
+            default:
+                return super.onContextItemSelected(item);
         }
 
-        return super.onContextItemSelected(item);
     }
 
      public void displayToast(String msg){
