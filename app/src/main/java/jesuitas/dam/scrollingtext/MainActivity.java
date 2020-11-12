e0f4e175dd87ab888f43f76417693e2c507411c1
Miguel Rodriguez <daw011.2020@gmail.com>
2020-11-12 13:33:03 +0100

    añadiendo la action bar contextual

--- app/src/main/java/jesuitas/dam/scrollingtext/MainActivity.java
+++ app/src/main/java/jesuitas/dam/scrollingtext/MainActivity.java
@@ -1,14 +1,56 @@
 package jesuitas.dam.scrollingtext;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView articulo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Asigno el menu contextual a esta View
+        articulo=findViewById(R.id.article);
+        registerForContextMenu(articulo);
+
     }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater= getMenuInflater();
+        inflater.inflate(R.menu.menu_context, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.edit:
+                displayToast(getString(R.string.edit_text));
+                return true;
+            case R.id.Share:
+                displayToast(getString(R.string.share_text));
+                return true;
+            case R.id.Delete:
+                displayToast(getString(R.string.delete_text));
+
+        }
+
+        return super.onContextItemSelected(item);
+    }
+
+     public void displayToast(String msg){
+        Toast.makeText(getApplicationContext(),msg,Toast.LENGTH_SHORT).show();
+     }
 }
